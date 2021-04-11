14a2e1839d7171d27c99c506c296bbcd99cf5f67
Sarah Boyce <sarahvboyce95@gmail.com>
2021-04-11 12:05:59 +0100

    feat: add the Game list url to the navbar

--- games/urls.py
+++ games/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GameListView.as_view(), name='games'),
+    path('', views.GameListView.as_view(), name='game_list'),
 ]
